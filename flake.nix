cd60672f3097864e66d69c82555e9e53db753481
Alex Ryder <powerskater3@gmail.com>
2026-02-10 18:50:48 +0000

    Initial commit.

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A flake defining locaT-DNA dependencies";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [];
+          packages = with pkgs; [
+            nodejs_24
+            pnpm
+          ];
+        };
+      }
+    );
+}
